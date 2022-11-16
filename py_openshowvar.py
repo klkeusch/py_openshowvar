b226bef9bb07c8848ec9af80d8736cbecfa34e06
Klaus Keusch <hallo@klauskeusch.de>
2022-11-16 11:27:56 +0100

    Debug off. Values saved in file stripped from unwanted chars. Clean-up.

--- py_openshowvar.py
+++ py_openshowvar.py
@@ -214,7 +214,7 @@ def run_shell(ip, port):
                     parts = data.split(',')
                     if len(parts) == 1:
                         extracted_var_value = client.read(data.strip(), True)
-                        print(extracted_var_value)
+                        print("\n{}\n".format(extracted_var_value))
                         f.write("Abgefragte Variable: {}, Wert: {} um {}\n".format(parts, extracted_var_value,
                                                                                    time.ctime()))
                     else:
