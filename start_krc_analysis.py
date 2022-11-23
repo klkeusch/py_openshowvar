1d8e7c0c4580c803e3f99615ea766213fd30a026
Klaus Keusch <hallo@klauskeusch.de>
2022-11-23 11:50:19 +0100

    Second command line window for displaying current values

--- start_krc_analysis.py
+++ start_krc_analysis.py
@@ -1,5 +1,5 @@
 import os
 import sys
 
-os.system("start /B start cmd.exe @cmd /k ping 8.8.8.8 -t ")
-os.system("start /B start cmd.exe @cmd /k python py_openshowvar.py")
+os.system("start /B start cmd.exe @cmd /c python py_openshowvar.py ")
+os.system("start /B start cmd.exe @cmd /c python py_krc_dashboard.py")
